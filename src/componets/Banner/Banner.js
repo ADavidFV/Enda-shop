e618a3e87c75889306b2529c1aec503223dd57b4
ADavidFV <alandfv02@gmail.com>
2023-10-01 16:26:34 -0600

    firs commit

---
+++ src/componets/Banner/Banner.js
@@ -0,0 +1,13 @@
+import "./Banner.css";
+const Banner = () => {
+  return (
+    <div className='banner'>
+      <div className='banner-container'>
+        <h1>ENDA Shop</h1>
+        <p>Tu mejor opción en ventas</p>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
